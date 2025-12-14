a51946972612e28890b710a8380e10099eaa53fe
Alisa Pokormlyak <apokormlyak@yandex.ru>
2025-12-14 10:16:21 +0300

    добавила инкапсуляцию полям класса worker

--- worker.py
+++ worker.py
@@ -1,8 +1,8 @@
 class Worker:
     def __init__(self, fio: str, salary: str, job_name: str, employment_year: str):
-        self.fio = fio
-        self.salary = salary
-        self.job_name = job_name
+        self._fio = fio
+        self._salary = salary
+        self._job_name = job_name
         self._employment_year = employment_year
 
     def __str__(self):
@@ -11,3 +11,15 @@ class Worker:
     @property
     def employment_year(self):
         return self._employment_year
+
+    @property
+    def salary(self):
+        return self._salary
+
+    @property
+    def fio(self):
+        return self._fio
+
+    @property
+    def job_name(self):
+        return self._job_name
